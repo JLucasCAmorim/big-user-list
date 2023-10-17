7f2983044deca27510e06ebc4235c1aa25ee504c
John Amorim <jlucasevangelista153@gmail.com>
2023-10-17 00:30:45 +0000

    Updating request

--- src/pages/api/data.ts
+++ src/pages/api/data.ts
@@ -2,22 +2,19 @@ import { NextApiRequest, NextApiResponse } from "next"
 import axios from "axios"
 import { UserAPIResponse } from "../../modules/users/dtos/user.dto"
 
-export default function api(req: NextApiRequest, res: NextApiResponse) {
-	axios
-		.get("https://randomuser.me/api/?results=5000", {
-			headers: {
-				'Content-type': 'application/json'
-			}
-		})
-		.then((response) => {
-			console.log(response)
-			return response.data
-		})
-		.then((data: UserAPIResponse) => {
-			console.log(data)
-			return res.json(data)
-		})
-		.catch((error) => console.error(error)); 
+export default async function api(req: NextApiRequest, res: NextApiResponse) {
+	try {
+	const response = await axios.get("https://randomuser.me/api/?results=5000", {
+		headers: {
+			'Content-type': 'application/json'
+		}
+	})
 
-	return
+	console.log(response)
+
+	return res.json(response.data)
+	} catch(error) {
+		console.error(error)
+		throw new Error("Request failed.")
+	}
 }
